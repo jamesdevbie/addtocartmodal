32d0ffdd49a0b3b755aa2c8b4bd212efa46a78b2
jamesdevbie <jamesdevbie@gmail.com>
2024-10-01 12:20:03 +0530

    Updated table data styles

--- src/Components/CartModal/CartModal.jsx
+++ src/Components/CartModal/CartModal.jsx
@@ -48,19 +48,21 @@ const CartModal = ({ cartList, setCartList, setIsModalOpened }) => {
                     <img className="cart-image" src={cl.image} alt={cl.title} />
                   </td>
                   <td>{cl.title}</td>
-                  <td>
+                  <td style={{ letterSpacing: '-1px', textWrap: 'nowrap' }}>
                     {' '}
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      height="15px"
+                      height="12px"
                       viewBox="0 -960 960 960"
-                      width="15px"
+                      width="12px"
                       fill="#03033d"
-                      style={{ paddingTop: '10px' }}
+                      style={{
+                        paddingTop: '10px',
+                      }}
                     >
                       <path d="M549-120 280-400v-80h140q53 0 91.5-34.5T558-600H240v-80h306q-17-35-50.5-57.5T420-760H240v-80h480v80H590q14 17 25 37t17 43h88v80h-81q-8 85-70 142.5T420-400h-29l269 280H549Z" />
                     </svg>
-                    {cl.price}
+                    <span>{cl.price}</span>
                   </td>
                   <td>
                     <button
